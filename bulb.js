bf43b73d6fa0ec7e864812bf09ddcb307d057827
Raimon T Simon <raimonrts@gmail.com>
2022-08-02 18:52:28 +0530

    Added Bulb page

---
+++ bulb.js
@@ -0,0 +1,8 @@
+function bulbOn(){
+    document.getElementById("bulbimage").src="/bulb-images/pic_bulbon.gif";
+    console.log("bulb is on");
+}
+function bulbOff(){
+    document.getElementById("bulbimage").src="/bulb-images/pic_bulboff.gif";
+    console.log("bulb is off")
+}
