8de8f989f2f0627b6d7331632189bfe88a3a2539
Raimon T Simon <raimonrts@gmail.com>
2022-08-02 19:18:24 +0530

    bulb images changed to url

--- bulb.js
+++ bulb.js
@@ -1,8 +1,8 @@
 function bulbOn(){
-    document.getElementById("bulbimage").src="/bulb-images/bulb-on.png";
+    document.getElementById("bulbimage").src="https://i.postimg.cc/6QyTynzr/bulb-on.png";
     console.log("bulb is on");
 }
 function bulbOff(){
-    document.getElementById("bulbimage").src="/bulb-images/bulb-off.png";
+    document.getElementById("bulbimage").src="https://i.postimg.cc/KjK1wL3c/bulb-off.png";
     console.log("bulb is off")
 }
